ddb2a3acf7e8ce31d82f1ed2902d9df625bc5631
rsudar <rsudar@BLRRSUDAR60654>
2016-07-22 10:41:10 +0530

    Add project files.

---
+++ Grades/GradeBook.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class GradeBook
+    {
+        List<float> Grades;
+        public GradeBook()
+        {
+            Grades = new List<float>();
+        }
+
+        public void AddGrade(float grade) {
+            Grades.Add(grade);
+        }
+
+        public GradeStatistics computeStatistics()
+        {
+            GradeStatistics stats = new GradeStatistics();
+            float sum = 0;
+            stats.LowestGrade = Grades.First();
+            foreach (float grade in Grades)
+            {
+                
+                stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
+                stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
+                sum = sum + grade;
+            }
+                stats.AverageGrade = sum/Grades.Count;
+                return stats;
+        }
+    }
+}
---
+++ Grades/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(87);
+            book.AddGrade(90.98f);
+            book.AddGrade(76);
+
+            GradeBook book2 = new GradeBook();
+            book2.AddGrade(79);
+            book2.AddGrade(78);
+
+            GradeBook book3 = book2;
+            book3.AddGrade(65);
+
+            GradeStatistics stats = book.computeStatistics();
+
+            Console.WriteLine(stats.HighestGrade);
+            Console.WriteLine(stats.LowestGrade);
+            Console.WriteLine(stats.AverageGrade);
+        }
+    }
+}
